724df00b146f72dbc63c4f5248f905a3ae89ea85
Matheus França <99504777+Matheus-Franca-x@users.noreply.github.com>
2023-11-22 01:20:33 -0300

    Update 9_ex9-exerc9.sql

--- 9_ex9-exerc9.sql
+++ 9_ex9-exerc9.sql
@@ -78,7 +78,7 @@ INSERT INTO compra VALUES
 (15054,10008,1,95.00,'06/08/2021')
 
 
---1
+--1 Nome, valor unitário, nome da editora e nome do autor dos livros do estoque que foram vendidos. Não podem haver repetições.
 SELECT e.nome, e.valor, ed.nome, a.nome
 FROM estoque e, editora ed, autor a, compra c
 WHERE e.codEditora = ed.codigo
@@ -86,13 +86,13 @@ WHERE e.codEditora = ed.codigo
 	AND c.codEstoque = e.codigo
 GROUP BY e.nome, e.valor, ed.nome, a.nome
 
---2
+--2 Nome do livro, quantidade comprada e valor de compra da compra 15051.
 SELECT e.nome, c.qtdComprada, c.valor
 FROM compra c, estoque e
 WHERE c.codEstoque = e.codigo
 	AND c.codigo = 15051
 
---3
+--3 Nome do livro e site da editora dos livros da Makron books (Caso o site tenha mais de 10 dígitos, remover o www.).
 SELECT 
 e.nome, 
 CASE WHEN LEN(ed.site) > 10
@@ -103,31 +103,31 @@ FROM estoque e, editora ed
 WHERE e.codEditora = ed.codigo
 	AND ed.nome LIKE 'Makron books'
 
---4
+--4 Nome do livro e Breve Biografia do David Halliday.
 SELECT e.nome, a.biografia
 FROM estoque e, autor a
 WHERE e.codAutor = a.codigo
 	AND a.nome LIKE 'David Halliday'
 
---5
+--5 Código de compra e quantidade comprada do livro Sistemas Operacionais Modernos.
 SELECT c.codigo, c.qtdComprada
 FROM compra c, estoque e
 WHERE c.codEstoque = e.codigo
 	AND e.nome LIKE 'Sistemas Operacionais Modernos'
 
---6
+--6 Quais livros não foram vendidos.
 SELECT *
 FROM estoque e
 LEFT JOIN compra c ON e.codigo = c.codEstoque
 WHERE c.codigo IS NULL
 
---7
+--7 Quais livros foram vendidos e não estão cadastrados.
 SELECT *
 FROM estoque e
 LEFT JOIN compra c ON e.codigo = c.codEstoque
 WHERE c.codigo IS NOT NULL
 
---8
+--8 Nome e site da editora que não tem Livros no estoque (Caso o site tenha mais de 10 dígitos, remover o www.).
 SELECT 
 ed.nome,
 CASE WHEN LEN(ed.site) > 10
@@ -138,7 +138,7 @@ FROM editora ed
 LEFT JOIN estoque e ON e.codEditora = ed.codigo
 WHERE e.codigo IS NULL
 
---9
+--9 Nome e biografia do autor que não tem Livros no estoque (Caso a biografia inicie com Doutorado, substituir por Ph.D.).
 SELECT 
 a.nome, 
 CASE WHEN SUBSTRING(a.biografia, 1, 9) LIKE 'Doutorado'
@@ -149,26 +149,31 @@ FROM autor a
 LEFT JOIN estoque e ON e.codAutor = a.codigo
 WHERE e.codigo IS NULL
 
---10
+--10 O nome do Autor, e o maior valor de Livro no estoque. Ordenar por valor descendente.
 SELECT a.nome, e.valor
 FROM autor a, estoque e
 WHERE a.codigo = e.codAutor
 ORDER BY e.valor DESC
 
---11
+--11 O código da compra, o total de livros comprados e a soma dos valores gastos. Ordenar por Código da Compra ascendente.
 SELECT codigo, SUM(qtdComprada) AS qntd_comprada, SUM(valor) AS valor_total
 FROM compra
 GROUP BY codigo
 ORDER BY codigo ASC
 
---12
+--12 O nome da editora e a média de preços dos livros em estoque.Ordenar pela Média de Valores ascendente.
 SELECT ed.nome, AVG(e.valor) AS media_valor
 FROM editora ed, estoque e
 WHERE e.codEditora = ed.codigo
 GROUP BY ed.nome
 ORDER BY media_valor ASC
 
---13
+--13 O nome do Livro, a quantidade em estoque o nome da editora, o site da editora (Caso o site tenha mais de 10 dígitos, remover o www.), 
+--   criar uma coluna status onde:
+--     I. Caso tenha menos de 5 livros em estoque, escrever Produto em Ponto de Pedido.
+--    II. Caso tenha entre 5 e 10 livros em estoque, escrever Produto Acabando.
+--   III. Caso tenha mais de 10 livros em estoque, escrever Estoque Suficiente.
+--    IV. A Ordenação deve ser por Quantidade ascendente.
 SELECT e.nome, e.quantidade, ed.nome, 
 CASE WHEN LEN(ed.site) > 10
 	THEN SUBSTRING(ed.site, 5, 25)
@@ -187,7 +192,9 @@ FROM estoque e, editora ed
 WHERE e.codEditora = ed.codigo
 ORDER BY e.quantidade ASC
 
---14
+--14 Para montar um relatório, é necessário montar uma consulta com a seguinte saída: 
+--   Código do Livro, Nome do Livro, Nome do Autor, Info Editora (Nome da Editora + Site) de todos os livros.
+--   I. Só pode concatenar sites que não são nulos.
 SELECT e.codigo, e.nome, a.nome,
 CASE WHEN ed.site IS NULL
 	THEN ed.nome
@@ -198,11 +205,11 @@ WHERE e.codAutor = a.codigo
 	AND e.codEditora = ed.codigo
 
 
---15
+--15 Codigo da compra, quantos dias da compra até hoje e quantos meses da compra até hoje.
 SELECT codigo, DATEDIFF(DAY, dataCompra, GETDATE()) AS dias_compra, DATEDIFF(MONTH, dataCompra, GETDATE()) AS mes_compra
 FROM compra
 
---16
+--16 O código da compra e a soma dos valores gastos das compras que somam mais de 200.00.
 SELECT codigo, SUM(valor)
 FROM compra
 GROUP BY codigo
